0e4e0faf6198106118338889fa378fe3e18cd9f8
Jayden Lee <jayden@Jaydens-MacBook-Pro.local>
2020-10-08 02:31:24 +0100

    hello world

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-console.log("Testing")
+const quoteContainer  = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+// Get quote from API
+
+async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // Reduce font size for longer quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+
+        // If author name is blank, display 'Unknown Author'
+        if (data.quoteAuthor === "") {
+            authorText.innerText = "Unknown Author";
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        
+    } catch (error) {
+        getQuote(); // if it encounters an issue at getting a quote, request for another one
+        
+    }
+
+}
+
+// Tweet quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = quoteAuthor.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', getQuote());
+twitterBtn.addEventListener('click', tweetQuote())
+
+// On load
+getQuote();
